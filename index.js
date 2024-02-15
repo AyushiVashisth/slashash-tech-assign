899af1f16eb8dfde802fbfd937700be1890afde3
Ayushi Vashisth <ayushivashisth22@gmail.com>
2024-02-15 13:33:28 +0530

    add indexjs  routes

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const cors = require("cors");
 const connection = require("./config/db");
 require("dotenv").config(); 
 
+const jokeRoutes = require('./routes/jokeRoutes');
+const favoriteRoutes = require('./routes/favoriteRoutes');
+
 const app = express();
 
 app.use(express.json());
@@ -16,6 +19,9 @@ app.get("/", (req, res) => {
   res.status(200).send("Welcome to slashash-tech-assign backend api");
 });
 
+// Routes
+app.use('/api/jokes', jokeRoutes);
+app.use('/api/favorites', favoriteRoutes);
 
 
 app.listen(process.env.PORT, async () => {
